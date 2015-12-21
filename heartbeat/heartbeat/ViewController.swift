96bed0273c0128041e8e1e47eb3807e73d0707a2
Robert Szumlakowski <rszumlakowski@pivotal.io>
2015-12-21 13:33:46 -0500

    Created iOS project in xCode
    Signed-off-by: Ryan M Collins <rcollins@pivotal.io>

---
+++ heartbeat/heartbeat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  heartbeat
+//
+//  Created by DX173-XL on 2015-12-21.
+//  Copyright © 2015 Pivotal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
