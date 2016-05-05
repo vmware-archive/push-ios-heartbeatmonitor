17ba7b6b731064b0ccb8c6c4096c94a88101f138
Ryan Collins <rcollins@pivotal.io>
2016-05-05 14:18:42 -0400

    RobS - [#119029725] Added commands to the build script to change the environment name if the ENV environment variable is set.
    Signed-off-by: Ryan Collins <rcollins@pivotal.io>

--- scripts/build-swift-ipa.sh
+++ scripts/build-swift-ipa.sh
@@ -2,6 +2,34 @@
 
 set -ex
 
+ENV=$1
+
+if [ "$ENV" != "" ]; then
+
+  echo "### Modifying build for the $ENV environment."
+
+  # Replace the environment name in the Info.plist file
+  sed -i "" "/<key>NSExceptionDomains<\\/key>/{ # Find the NSExceptionDomains key
+
+    # Skip past two lines
+    n;n
+
+    # Change the next line
+    c\\
+    \\                       <key>push-api.${ENV}.cf-app.com</key>
+    }" heartbeat/heartbeat/Info.plist
+
+  # Replace the environment name in the Pivotal file
+  sed -i "" "/<key>pivotal.push.serviceUrl<\\/key>/{ # Find the NSExceptionDomains key
+    # Skip to the next line
+    n
+
+    # Change the next line
+    c\\
+    \\       <string>https://push-api.${ENV}.cf-app.com</string>
+    }" heartbeat/heartbeat/Pivotal.plist
+fi
+
 # Produce the build archive
 xcodebuild \
   -project "heartbeat/PCF Push Heartbeat Monitor.xcodeproj" \
